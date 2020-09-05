f0c72c6449ce5e29afabc74fb5cffa23076e7f37
Marin Freud <freudmarin@gmail.com>
2020-09-05 14:11:30 +0200

    Initial

---
+++ source/repos/UserManagement/UserManagement/Identity/ApplicationDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace UserManagement.Identity
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext() : base("DefaultConnection")
+        {
+        }
+
+      //  public System.Data.Entity.DbSet<UserManagement.Identity.ApplicationUser> ApplicationUsers { get; set; }
+
+        //  public System.Data.Entity.DbSet<UserManagement.Identity.ApplicationUser> ApplicationUsers { get; set; }
+
+
+
+        //nuk duhet ,gjenerohet vete // public System.Data.Entity.DbSet<UserManagement.Identity.ApplicationUser> ApplicationUsers { get; set; }
+    }
+}
+
+
+
---
+++ source/repos/UserManagement/UserManagement/Identity/ApplicationUser.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace UserManagement.Identity
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public DateTime? Birthday { get; set; }
+        public string Address { get; set; }
+
+        public string name { get; set; }
+
+    }
+}
